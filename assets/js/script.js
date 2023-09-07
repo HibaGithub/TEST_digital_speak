5de52a0ff296c577a74eb1fac3aa000904984b92
hibarhazlani <hibarhzlani@gmail.com>
2023-09-07 19:35:14 +0100

    dans cette commit je suis ajouter trois fichier css image et js

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//  cette function qui Permettez-nous de tourner la phrase de learn more 
+const circleTypeTop = new CircleType(
+  document.getElementById("learn-more-top")
+);
+const circleTypeBottom = new CircleType(
+  document.getElementById("learn-more-bottom")
+);
+circleTypeTop.radius(50).dir(-1);
+circleTypeBottom.radius(50).dir(-1);
+
+
+
+
