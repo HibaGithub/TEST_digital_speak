4c0bb78850808120b73bb6c1b9c74ed86f589cd3
hibarhazlani <hibarhzlani@gmail.com>
2023-09-07 20:30:55 +0100

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,25 @@ circleTypeTop.radius(50).dir(-1);
 circleTypeBottom.radius(50).dir(-1);
 
 
+// je suis utiluse Slick Slider cette une 
 
 
+    $('.slider').slick({
+      centerMode: true,
+      dots: true,
+      autoplay: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          Infinity: true,
+          slidesToShow: 1
+        },
+      }]
+    });
+
+ 
